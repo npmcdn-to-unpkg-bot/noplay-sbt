1b7bda1cd500886b330746f341a3785b2a98bd56
Frédéric Nowak <frederic.nowak@gmail.com>
2016-04-27 11:52:59 +0200

    Change SbtWebIndex package.

--- noplay-sbt-web/src/main/scala/io/noplay/sbt/SbtWebIndex.scala
+++ noplay-sbt-web/src/main/scala/io/noplay/sbt/SbtWebIndex.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.alphard.sbt
+package io.noplay.sbt
 
 import java.net.URI
 import java.nio.charset.Charset
