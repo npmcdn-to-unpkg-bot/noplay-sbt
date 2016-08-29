5f5a64100838171fe7a07e3703172a677ebc14c2
Frédéric Nowak <frederic.nowak@gmail.com>
2016-08-30 00:26:42 +0200

    Refactor SbtDeployS3 plugin

--- noplay-sbt-web/src/main/scala/io/noplay/sbt/web/aws/SbtDeployS3.scala
+++ noplay-sbt-web/src/main/scala/io/noplay/sbt/web/aws/SbtDeployS3.scala
@@ -15,48 +15,33 @@
  */
 package io.noplay.sbt.web.aws
 
-import com.amazonaws.services.s3.model.CannedAccessControlList
-import io.alphard.sbt.aws.SbtAws.autoImport._
-import io.alphard.sbt.aws.SbtS3
-import io.alphard.sbt.aws.SbtS3.autoImport._
 import com.typesafe.sbt.web.SbtWeb.autoImport.WebKeys._
 import com.typesafe.sbt.web._
+import io.alphard.sbt.aws.SbtAws
+import io.alphard.sbt.aws.SbtAws.autoImport._
+import io.alphard.sbt.aws.SbtS3.autoImport._
 import sbt.Keys._
 import sbt._
 
 object SbtDeployS3
   extends AutoPlugin {
 
-  override lazy val requires = SbtWeb && SbtS3
+  override lazy val requires = SbtWeb && SbtAws
 
   object autoImport {
     val DeployS3 = config("deploy-s3").hide
     val DeployS3Realm = "Deploy S3 Realm"
-
-    val deployS3Realm = settingKey[String]("S3 realm")
-    val deployS3BucketName = taskKey[String]("S3 bucket name")
-    val deployS3IndexName = settingKey[String]("S3 index name")
-    val deployS3ErrorName = settingKey[String]("S3 error name")
-    val deployS3 = taskKey[Seq[URL]]("Deploy the staging directory content to an s3 bucket")
+    val deployS3 = taskKey[Seq[(File, URI)]]("Deploy the staging directory content to an s3 bucket")
   }
 
   import SbtDeployS3.autoImport._
 
   override lazy val projectSettings =
-    inConfig(DeployS3)(awsSettings) ++
-      inConfig(DeployS3)(s3Settings) ++ Seq(
-      deployS3Realm := DeployS3Realm,
-      deployS3BucketName := name.value,
-      deployS3IndexName := "index.html",
-      deployS3ErrorName := "error.html",
-      deployS3 <<= s3Sync in DeployS3,
-      awsRealm in DeployS3 <<= deployS3Realm,
-      s3Cacl in DeployS3 := CannedAccessControlList.PublicRead,
-      s3BucketName in DeployS3 <<= deployS3BucketName,
-      s3Mapping in DeployS3 := {
-        val directory = stagingDirectory.value
-        val files = IO.listFiles(stage.value)
-        io.alphard.sbt.util.IO.mapAllFiles(Seq(directory), files).toSeq
-      }
+    inTask(deployS3)(awsSettings) ++
+      inTask(deployS3)(s3Settings) ++ Seq(
+      awsRealm in deployS3 := DeployS3Realm,
+      s3Directory in deployS3 := stagingDirectory.value,
+      s3BucketName in deployS3 := name.value,
+      deployS3 := (s3Sync in deployS3).dependsOn(stage).value
     )
 }
